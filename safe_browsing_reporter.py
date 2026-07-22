69a0c5fc25e9388ad70e37ae6d6fe2ff7a81532e
smhozt <semihozturk93@gmail.com>
2026-07-22 12:06:00 +0300

    Update active domain for turkbet

--- safe_browsing_reporter.py
+++ safe_browsing_reporter.py
@@ -26,7 +26,7 @@ BRANDS = {
         "active_domains": ["betsat.com", "betsat1605.com"],
     },
     "turkbet": {
-        "active_domains": ["turkbet.io", "745turkbet.com"],
+        "active_domains": ["turkbet.io", "749turkbet.com"],
     },
 }
 
